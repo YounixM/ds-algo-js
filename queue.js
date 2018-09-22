055123e4b6051651ca34911571271b5ef48b300b
Yunus M <myounis.ar@live.com>
2018-09-23 01:47:24 +0530

    Implementing queue and priority queue class in javascript

---
+++ queue.js
@@ -0,0 +1,63 @@
+/* Queues */
+
+function Queue () { 
+    collection = [];
+    this.print = function() {
+        console.log(collection);
+    };
+    this.enqueue = function(element) {
+        collection.push(element);
+    };
+    this.dequeue = function() {
+        return collection.shift(); 
+    };
+    this.front = function() {
+        return collection[0];
+    };
+    this.size = function() {
+        return collection.length; 
+    };
+    this.isEmpty = function() {
+        return (collection.length === 0); 
+    };
+}
+
+
+/* Priority Queue */
+
+function PriorityQueue () {
+    var collection = [];
+    this.printCollection = function() {
+      (console.log(collection));
+    };
+    this.enqueue = function(element){
+        if (this.isEmpty()){ 
+            collection.push(element);
+        } else {
+            var added = false;
+            for (var i=0; i<collection.length; i++){
+                 if (element[1] < collection[i][1]){ //checking priorities
+                    collection.splice(i,0,element);
+                    added = true;
+                    break;
+                }
+            }
+            if (!added){
+                collection.push(element);
+            }
+        }
+    };
+    this.dequeue = function() {
+        var value = collection.shift();
+        return value[0];
+    };
+    this.front = function() {
+        return collection[0];
+    };
+    this.size = function() {
+        return collection.length; 
+    };
+    this.isEmpty = function() {
+        return (collection.length === 0); 
+    };
+}
